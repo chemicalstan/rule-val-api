61dec5d5345bc80532b85e77f46c6f010416f99c
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-27 11:56:57 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const baseRoute = require('./routes/baseRoute');
+const validateRoute = require('./routes/validateRoute');
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(baseRoute);
+app.use(validateRoute);
+
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
