6c765b33ae19d42287d6ff38889d06a66d7108a4
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-28 18:03:07 +0100

    Integrate reusable response methods

--- helpers/responses.js
+++ helpers/responses.js
@@ -1,26 +1,59 @@
-const successRes = {
-  message,
-  status: "success",
-  data
+const { errorRes, successRes, resData } = require("../helpers/messages");
+/**
+ *  Success (200)
+ * @param {objObjectect} messageObj message object
+ * @param {Object} res response object
+ * @returns {Object} OK
+ */
+const success = async (messageObj, res) => {
+  return await res.status(200).json(messageObj);
 };
-const errorRes = {
-  message,
-  status: "error",
-  data
+/**
+ * Bad Response (400)
+ * @param {Object} messageObj message object
+ * @param {Object} res response object
+ * @returns {Object} Bad Response
+ */
+const error = async (messageObj, res) => {
+  return await res.status(400).json(messageObj);
 };
 
-const resData = {
-  validation: {
-    error,
-    field,
-    field_value,
-    condition,
-    condition_value
-  }
-}
+/**
+ * Validation Successful
+ * @param {Object} req
+ * @param {Object} res
+ * @returns {Object} OK
+ */
+const successfulValidation = async (req, res) => {
+  const { rule, data } = req.body;
+  successRes.message = `field ${rule.field} successfully validated.`;
+  resData.validation.field = rule.field;
+  resData.validation.field_value = data[rule.field];
+  resData.validation.condition = rule.condition;
+  resData.validation.condition_value = rule.condition_value;
+  return await success(successRes, res);
+};
+
+/**
+ * Validation Failed
+ * @param {Object} req
+ * @param {Object} res
+ * @returns {Object} Bad Response
+ */
+const failedValidation = async (req, res) => {
+  const { rule, data } = req.body;
+  errorRes.message = `field ${rule.field} failed validation.`;
+  errorRes.data = resData;
+  resData.validation.field = rule.field;
+  resData.validation.field_value = data[rule.field];
+  resData.validation.condition = rule.condition;
+  resData.validation.condition_value = rule.condition_value;
+  return await error(errorRes, res);
+};
 
 module.exports = {
-  successRes,
-  errorRes,
-  resData
+  success,
+  error,
+  successfulValidation,
+  failedValidation
 };
