2d2a4a11f5ea244a962716914d75714fdcedf420
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-28 18:00:05 +0100

    Handle server error

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const baseRoute = require("./routes/baseRoute");
 const validateRoute = require("./routes/validateRoute");
+const { errorRes } = require("./helpers/messages");
 require("dotenv").config();
 
 const app = express();
@@ -9,6 +10,20 @@ const app = express();
 app.use(bodyParser.json());
 app.use(baseRoute);
 app.use(validateRoute);
+
+// Handle application errors
+app.use((error, req, res, next) => {
+  res.status(error.statusCode || 500);
+  errorRes.message = "Invalid JSON payload passed.";
+  errorRes.data = null;
+  res.json(errorRes);
+});
+
+// Handle 404 - Keep this as a last route
+app.use(function(req, res, next) {
+  res.status(404);
+  res.send("404: File Not Found");
+});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
